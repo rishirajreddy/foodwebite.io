d045679f674d2b0c891f14a9e29e134af0235a8a
rishirajreddy <64702890+rishirajreddy@users.noreply.github.com>
2020-11-21 13:09:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+    var x= document.getElementById("check-class");
+        function slideshow() {
+            if(x.style.display === "none") {
+
+            x.style.display ="block"
+        }
+          else {
+            x.style.display = "none";
+
+        }
+
+    }
+
+    
+
